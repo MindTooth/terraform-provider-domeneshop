f3220f4b7022360c1506cdeabe2ec8f9cdb3c4f5
Birger J. Nordølum <contact@mindtooth.no>
2019-10-06 16:46:38 +0200

    Begin fresh

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"github.com/MindTooth/terraform-provider-domeneshop/domeneshop"
-	"github.com/hashicorp/terraform/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: domeneshop.Provider})
+		ProviderFunc: func() terraform.ResourceProvider {
+			return Provider()
+		},
+	})
 }

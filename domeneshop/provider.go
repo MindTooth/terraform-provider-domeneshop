510d5876bfea9a20ae2a6c97eb2200b8a1a8a02c
Birger J. Nordølum <contact@mindtooth.no>
2020-09-25 10:07:21 +0200

    temp: before merge

--- domeneshop/provider.go
+++ domeneshop/provider.go
@@ -28,6 +28,7 @@ func Provider() terraform.ResourceProvider {
 		},
 		/*
 			ResourcesMap: map[string]*schema.Resource{
+				"domeneshop_forward": resourceDomeneshopForward(),
 				"domeneshop_record": resourceDomeneshopRecord(),
 			},
 		*/
